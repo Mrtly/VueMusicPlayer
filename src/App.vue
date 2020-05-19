da65505f9e2620e2b3332f05636123a28a03bacb
Mrtly <mrtly.ca@gmail.com>
2020-05-19 16:09:16 +0100

    can play & pause tune

--- src/App.vue
+++ src/App.vue
@@ -3,14 +3,22 @@
      <header>tunesss</header>
   <main>
     <section class="player">
-      <h2 class="title"> {{ current.title }} <span>{{ current.artist }}</span></h2>
+      <h2 class="title"> {{ current.title }} - <span>{{ current.artist }}</span></h2>
       <div class="control">
         <button class="prev"> Previous</button>
-        <button class="paus" v-if="!isPlaying"> Play</button>
-        <button class="play" v-else> Pause</button>
+        <button class="paus" v-if="!isPlaying" @click="play"> Play</button>
+        <button class="play" v-else @click="pause"> Pause</button>
         <button class="next"> Next</button>
-
       </div>
+    </section>
+    <section class="playlist">
+      <h4> playlist </h4>
+      <button v-for="song in songs" 
+      :key="song.src" 
+      @click="play(song)" 
+      :class="(song.src == current.src) ? 'song playing' : 'song'">
+      {{ song.title }} - {{ song.artist }}
+      </button>
     </section>
   </main>
   </div>
@@ -26,17 +34,31 @@ export default {
       index: 0,
       isPlaying: false,
       songs: [
-        {title: 'the richest man in Babylon',
+        {title: 'The richest man in Babylon',
         artist: 'Thievery Corporation',
         src: require('./assets/the_richest_man_in_babylon.mp3')
         },
-        {title: 'until the morning',
+        {title: 'Until the morning',
         artist: 'Thievery Corporation',
-        src: require('./assets/until_the_morning-troni.mp3')
+        src: require('./assets/until_the_morning.mp3')
         }
       ],
       player: new Audio()
     }
+  },
+  methods: {
+    play(song){
+      if (typeof song.src != "undefined") {
+        this.current = song;
+        this.player.src = this.current.src;
+      }
+      this.player.play();
+      this.isPlaying = true;
+    },
+    pause () {
+      this.player.pause();
+      this.isPlaying = false;
+    },
   },
   created () {
     this.current = this.songs[this.index];
